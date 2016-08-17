5075f6da9ba170b394f1f1ba0145d5ac5743eaf2
Tim Paul <contact@timpaul.co.uk>
2016-08-17 13:27:53 +0100

    Apply GOV.UK header

--- source/javascripts/app/_js.js
+++ source/javascripts/app/_js.js
@@ -1,27 +1,30 @@
-// $(document).ready(function() {
-// var stickyNavTop = $('.tocify-wrapper').offset().top;
+$(document).ready(function() {
+var stickyNavTop = $('.tocify-wrapper').offset().top;
  
-// var stickyNav = function(){
-//	var scrollTop = $(window).scrollTop();
-//	var $el = $('#global-header');  //record the elem so you don't crawl the DOM everytime  
-//	var bottom = $el.position().top + $el.outerHeight(true) - scrollTop;
+var stickyNav = function(){
+	var scrollTop = $(window).scrollTop();
+	var $el = $('#breadcrumb');  //record the elem so you don't crawl the DOM everytime  
+	var bottom = $el.position().top + $el.outerHeight(true) - scrollTop;
   	      
-//	if (scrollTop > stickyNavTop) { 
-//	    $('.tocify-wrapper').addClass('sticky');
-//	} else {
-//		if (bottom > 0){
-//			$('.tocify-wrapper').css("top", bottom + "px");
-//			$('.dark-box').css("top", (bottom - 53) + "px");
-//		}
+	if (scrollTop > stickyNavTop) { 
+	    $('.tocify-wrapper').addClass('sticky');
+	    $('.tocify-wrapper').css("top", 0);
+	    $('.dark-box').addClass('sticky');
+	    $('.dark-box').css("top", 0);
+	} else {
+		if (bottom > 0){
+			$('.tocify-wrapper').css("top", bottom + "px");
+			$('.dark-box').css("top", (bottom - 107) + "px");
+		}
   		
-//	    $('.tocify-wrapper').removeClass('sticky'); 
-//	    $('.dark-box').removeClass('sticky'); 
-//	}
-// };
+	    $('.tocify-wrapper').removeClass('sticky'); 
+	    $('.dark-box').removeClass('sticky'); 
+	}
+};
  
-// stickyNav();
+stickyNav();
  
-// $(window).scroll(function() {
-//   stickyNav();
-// });
-// });
+$(window).scroll(function() {
+  stickyNav();
+});
+});

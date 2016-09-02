cde06eff3311f0eee19c27d77465eb5b12c78e78
Tim Paul <contact@timpaul.co.uk>
2016-09-02 12:53:39 +0100

    Tidy up JavaScript for doc title toggle

--- source/javascripts/app/_js.js
+++ source/javascripts/app/_js.js
@@ -1,35 +1,9 @@
 $(document).ready(function() {
-var stickyNavTop = $('.tocify-wrapper').offset().top;
- 
-var stickyNav = function(){
-	var scrollTop = $(window).scrollTop();
-	var $el = $('#breadcrumb');  //record the elem so you don't crawl the DOM everytime  
-	var bottom = $el.position().top + $el.outerHeight(true) - scrollTop;
-  	      
-	if (scrollTop > stickyNavTop) { 
-	    $('.tocify-wrapper').addClass('sticky');
-	    $('.tocify-wrapper').css("top", 0);
-	    $('.dark-box').addClass('sticky');
-	    $('.dark-box').css("top", 0);
-	} else {
-		if (bottom > 0){
-			$('.tocify-wrapper').css("top", (bottom - 15) + "px");
-			$('.dark-box').css("top", (bottom - 114) + "px");
-		}
-  		
-	    $('.tocify-wrapper').removeClass('sticky'); 
-	    $('.dark-box').removeClass('sticky'); 
-	}
-};
- 
-//stickyNav();
- 
-$(window).scroll(function() {
 
-});
 
+// Hide document title in site header if you're at the top of the page
+// This is to avoid duplication with the H1 of the documentation
 var $document = $(document);
-
 $('#proposition-name').addClass('visually-hidden');
 $document.scroll(function() {
 	$('#proposition-name').toggleClass('visually-hidden', $document.scrollTop() <= 60);
